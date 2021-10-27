09ec76fd9bfcbc7d6c88f01b08c44a1120dd1795
David Tomarov <Dtomarov@gmail.com>
2021-10-27 14:26:55 +0300

    Added LeNet5 class

--- main.py
+++ main.py
@@ -5,7 +5,59 @@ from torchvision import datasets
 import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
 
+import torch.nn as nn
+import torch.nn.functional as F
 
+
+#define the CNN architecture
+class LENET5(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        # convolutional layer (sees 28x28x1 image tensor)
+        self.conv1 = nn.Conv2d(1, 6, 5, stride=1, padding=2)
+        # convolutional layer (sees 14x14x16 tensor)
+        self.conv2 = nn.Conv2d(6, 16, 5, stride=1, padding=0)
+        # convolutional layer (sees 1X1x120 tensor)
+        self.conv3 = nn.Conv2d(16, 120, 5, stride=1, padding=0)
+        # max pooling layer
+        self.pool = nn.MaxPool2d(2, 2)
+        # linear layer (120 * 1 * 1 -> 84)
+        self.fc1 = nn.Linear(1 * 1 * 120, 84)
+        # linear layer (120 -> 10)
+        self.fc2 = nn.Linear(84, 10)
+        # dropout layer (p=0.25)
+        self.dropout = nn.Dropout(0.25)
+
+    def forward(self, x):
+        # add sequence of convolutional and max pooling layers
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+        # flatten image input
+        #x = x.view(-1, 64 * 4 * 4)
+        # add dropout layer
+        x = self.dropout(x)
+        # add 1st hidden layer, with relu activation function
+        x = F.relu(self.fc1(x))
+        # add dropout layer
+        x = self.dropout(x)
+        # add 2nd hidden layer, with relu activation function
+        x = self.fc2(x)
+        return x
+
+# create a complete CNN
+#model = LENET5()
+#print(model)
+
+# move tensors to GPU if CUDA is available
+# if train_on_gpu:
+#     model.cuda()
+#
+# if torch.cuda.is_available():
+#     device = torch.device('cuda')
+# else:
+#     device = torch.device('cpu')
+# print(device)
 # number of subprocesses to use for data loading
 num_workers = 0
 # how many samples per batch to load
