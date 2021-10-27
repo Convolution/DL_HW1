29356da15cdac62f1d2533225488f09919bcd1db
David Tomarov <Dtomarov@gmail.com>
2021-10-27 12:50:12 +0300

    test commit

--- main.py
+++ main.py
@@ -1,27 +1,45 @@
 import torch
-import torchvision
 import numpy as np
-import matplotlib as plt
-
+import torchvision
 from torchvision import datasets
 import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+
 
 # number of subprocesses to use for data loading
 num_workers = 0
 # how many samples per batch to load
 batch_size = 20
 
-# convert data to torch.FloatTensor
-transform = transforms.ToTensor()
+# Load data
+train_data = torchvision.datasets.FashionMNIST(
+    root = './data/FashionMNIST',
+    train = True,
+    download = True,
+    transform = transforms.Compose([
+        transforms.ToTensor()
+    ])
+)
 
-# choose the training and test datasets
-train_data = datasets.MNIST(root='data', train=True,
-                                   download=True, transform=transform)
-test_data = datasets.MNIST(root='data', train=False,
-                                  download=True, transform=transform)
+test_data = torchvision.datasets.FashionMNIST(
+    root = './data/FashionMNIST',
+    train = False,
+    download = True,
+    transform = transforms.Compose([
+        transforms.ToTensor()
+    ])
+)
 
 # prepare data loaders
-train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size,
-    num_workers=num_workers)
-test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size,
-    num_workers=num_workers)
+train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, num_workers=num_workers)
+test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, num_workers=num_workers)
+
+# obtain one batch of training images
+dataiter = iter(train_loader)
+images, labels = dataiter.next()
+images = images.numpy()
+
+img = images[0].squeeze()
+# Plot image
+plt.imshow(img, cmap='gray')
+plt.show()
